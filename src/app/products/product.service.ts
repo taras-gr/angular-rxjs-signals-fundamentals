ad7e16607406dfa4a12635605aaf4f0bed01ea86
taras-gr <tar.grytsenko@gmail.com>
2025-10-07 19:20:13 +0300

    Module 6: Handling HTTP Errors with Observables

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { Product } from './product';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,13 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        tap(s => console.log(s))
+        tap(s => console.log(s)),
+        catchError(err => this.handleError(err))
       );
   }
 
@@ -24,5 +27,10 @@ export class ProductService {
       .pipe(
         tap(s => console.log(s))
       );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    const formattedMessage = this.errorService.formatError(err);
+    return throwError(() => formattedMessage);
   }
 }
