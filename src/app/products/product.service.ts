c93e90460312879cc2ba1ce6853250bcb2ce100b
taras-gr <tar.grytsenko@gmail.com>
2025-10-08 19:42:47 +0300

    Module 7: Getting Related Data: switchMap, concatMap and mergeMap

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, concatMap, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,7 @@ export class ProductService {
 
   private http = inject(HttpClient);
   private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -21,12 +24,25 @@ export class ProductService {
       );
   }
 
-  getProduct(id: number) {
+  getProduct(id: number): Observable<Product> {
     const productUrl = this.productsUrl + '/' + id;
     return this.http.get<Product>(productUrl)
       .pipe(
-        tap(s => console.log(s))
+        tap(s => console.log(s)),
+        switchMap(product => this.getProductWithReviews(product)),
+        catchError(err => this.handleError(err))
       );
+  }
+
+  private getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          map(reviews => ({...product, reviews} as Product))
+        )
+    } else {
+      return of(product);
+    }
   }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
